db8890d66087fb851ad37f07f917c223f0efba77
Alex <alexkimani216@gmail.com>
2024-06-18 16:48:21 +0300

    first

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="Description" content="Enter your description here" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Outfit:100,200,300,regular,500,600,700,800,900"
+        rel="stylesheet" />
+    <title>Time</title>
+</head>
+<style>
+body {
+    background-color: #1a1919;
+    color: rgb(185, 178, 178);
+    font-family: outfit;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+}
+
+.time-date {
+    padding-left: 25%;
+
+}
+
+.time {
+    margin-top: 10rem;
+    box-shadow: -2px -2px 20px white, 2px 2px 10px black;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 5rem;
+}
+</style>
+
+<body>
+    <div class="col-12 col-sm-4 col-md-8 col-lg-11 time-date ">
+        <h1>TIME</h1>
+        <div class="col-6 time">
+
+            00:00:00
+
+        </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/js/bootstrap.min.js"></script>
+
+</body>
+<!-- use fuction to get time and date -->
+<?php
+function (){
+    new date
+    var h
+}
+?>
+
+</html>
